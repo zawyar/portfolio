0312dfb5df55cf6dd50fbd511d9ed61f49a03ce8
Zawyar Ur Rehman <zawyar8000@gmail.com>
2023-12-04 18:21:15 +0500

    basic version set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Zawyar Ur Rehman",
+    template: "%s | Zawyar Ur Rehman",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "A generalist software engineer with proven Full-Stack Development and DevOps chops",
   openGraph: {
-    title: "chronark.com",
+    title: "Zawyar Ur Rehman",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Zawyar Ur Rehman. Full-stack software engineer and DevOps engineer with a passion for using technology to solve interesting challenges and making a real world impact.",
+    url: "https://zawyar.vercel.app",
+    siteName: "Zawyar Ur Rehman",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://zawyar.vercel.app/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Zawyar Ur Rehman",
     card: "summary_large_image",
   },
   icons: {
